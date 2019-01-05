fc8c0b437cc1cc7f8db49df92cb3fd49e0f4006f
Mike Martin <2hip2carebear@gmail.com>
2019-01-05 15:38:29 -0800

    Fix for pip >= 10

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 
 install_reqs = parse_requirements('requirements.txt', session='hack')
